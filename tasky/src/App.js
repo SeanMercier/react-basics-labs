bbfe1d32bf9bde9d189ad55c966b68dc9b3ba0c1
seanm <20094677@mail.wit.ie>
2023-09-22 09:23:23 +0100

    Completed Week 2 Lab

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,16 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Task from './components/Task';
 
 function App() {
+  const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" },
+      { id: 4, title: "Do homework", description: "Web App Dev 2 Assignment", deadline: "After tomorrow"},
+      { id: 5, title: "Physical Activity", description: "Run 3 kilometers", deadline: "Sunday"}
+    ]
+  });
   return (
     <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" />
-      <Task title="Laundry" deadline="Tomorrow">
-        Fold laundry and put away
-      </Task>
-      <Task title="Tidy" deadline="Today" />
+      {taskState.tasks.map((task) => (              
+    <Task 
+      title={task.title}
+      description={task.description}
+      deadline={task.deadline}
+      key={task.id}
+    />
+  ))}
     </div>
   );
 }
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Task = (props) => {
     
     return (
-        <div className="card">
-            <p className="title">{props.title}</p>
+        <div className='card'>
+            <p className='title'>{props.title}</p>
             <p>Due: {props.deadline}</p>
-            <p>{props.children}</p>
+            <p className='description'>{props.description}</p>
         </div>
     )
 }
